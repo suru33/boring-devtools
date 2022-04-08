12d7844f821840f9391d1b96db07982be0d09719
SuRu <33urus@gmail.com>
2022-04-09 00:15:31 +0300

    created random string generator tool

--- src/components/tools/generators/StringsGenerator.tsx
+++ src/components/tools/generators/StringsGenerator.tsx
@@ -1,7 +1,68 @@
+import { Button, Checkbox, Group, NumberInput, Text, Textarea, TextInput } from "@mantine/core";
+import { useState } from "react";
+import { generateRandomStrings } from "../../../utils/string-utils";
+
 const StringsGenerator = () => {
-  return(
+  const [ upper, setUpper ] = useState(true);
+  const [ lower, setLower ] = useState(true);
+  const [ numeric, setNumeric ] = useState(true);
+  const [ symbols, setSymbols ] = useState(false);
+  const [ extras, setExtras ] = useState(false);
+  const [ pool, setPool ] = useState("");
+  const [ count, setCount ] = useState(1);
+  const [ length, setLength ] = useState(20);
+  const [ output, setOutput ] = useState("");
+
+  const generateOutput = (_: any) => {
+    const result = generateRandomStrings(length, upper, lower, numeric, symbols, extras ? pool : "", count);
+    setOutput(result);
+  };
+
+  return (
     <div>
-          RandomStringsGenerator
+      <Text size={"lg"} weight={700}>Character Set:</Text>
+      <Group direction="column" style={{ marginLeft: 25, marginTop: 15 }}>
+        <Checkbox
+          checked={upper}
+          label="Uppercase alphabets"
+          onChange={(e) => setUpper(e.currentTarget.checked)}/>
+        <Checkbox
+          checked={lower}
+          label="Lowercase alphabets"
+          onChange={(e) => setLower(e.currentTarget.checked)}/>
+        <Checkbox
+          checked={numeric}
+          label="Digits"
+          onChange={(e) => setNumeric(e.currentTarget.checked)}/>
+        <Checkbox
+          checked={symbols}
+          label="Symbols (!@#$%^&*()[])"
+          onChange={(e) => setSymbols(e.currentTarget.checked)}/>
+        <Group>
+          <Checkbox
+            checked={extras}
+            label="Extra characters"
+            onChange={(e) => setExtras(e.currentTarget.checked)}/>
+          <TextInput
+            placeholder="add more characters"
+            style={{ width: 300 }}
+            value={pool}
+            onChange={(e) => setPool(e.currentTarget.value)}/>
+        </Group>
+      </Group>
+      <Group style={{ marginTop: 15 }}>
+        <Text weight={700}>Length:</Text>
+        <NumberInput value={length} min={1} max={100} onChange={(val) => setLength(val)}/>
+        <Text weight={700}>Count: </Text>
+        <NumberInput value={count} min={1} max={20} onChange={(val) => setCount(val)}/>
+        <Button onClick={generateOutput}>Generate</Button>
+      </Group>
+      <Textarea
+        style={{ marginTop: 15 }}
+        minRows={20}
+        label="Output"
+        value={output}
+        onChange={(e) => setOutput(e.currentTarget.value)}/>
     </div>
   );
 };
---
+++ src/utils/string-utils.ts
@@ -0,0 +1,34 @@
+import * as _ from "lodash";
+import Chance from "chance";
+
+const random = new Chance(Math.random);
+
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numericLetters = "0123456789";
+const symbolLetters = "!@#$%^&*()[]";
+
+export const generateRandomString = (length: number, upper: boolean, lower: boolean, numeric: boolean, symbols: boolean, pool: string): string => {
+  let charSet = "";
+  if (upper) {
+    charSet += upperCaseLetters;
+  }
+  if (lower) {
+    charSet += lowerCaseLetters;
+  }
+  if (numeric) {
+    charSet += numericLetters;
+  }
+  if (symbols) {
+    charSet += symbolLetters;
+  }
+  if (_.isEmpty(pool.trim())) {
+    charSet += pool;
+    _.uniq(charSet).join("");
+  }
+  return random.string({ length: length, pool: charSet });
+};
+
+export const generateRandomStrings = (length: number, upper: boolean, lower: boolean, numeric: boolean, symbols: boolean, pool: string, count: number): string => {
+  return _.range(count).map(() => generateRandomString(length, upper, lower, numeric, symbols, pool)).join("\n");
+};
