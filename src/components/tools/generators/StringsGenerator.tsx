0cd89f9d80293d71ef82d590fad45e41181ae25d
SuRu <33urus@gmail.com>
2022-04-10 17:24:45 +0300

    changed to mantine hooks

--- src/components/tools/generators/StringsGenerator.tsx
+++ src/components/tools/generators/StringsGenerator.tsx
@@ -1,56 +1,35 @@
-import { Button, Checkbox, Group, NumberInput, Text, Textarea, TextInput } from "@mantine/core";
-import { useState } from "react";
+import { useInputState } from "@mantine/hooks";
+import { Button, Checkbox, Container, Group, NumberInput, Text, Textarea, TextInput } from "@mantine/core";
 import { generateRandomStrings } from "../../../utils/string-utils";
 import { asInputLabel, defaultMargin, verticalGroupIndent } from "../../../app-sx";
 
 const StringsGenerator = () => {
-  const defaultCount = 5;
-  const defaultLength = 20;
-  const [ upper, setUpper ] = useState(true);
-  const [ lower, setLower ] = useState(true);
-  const [ numeric, setNumeric ] = useState(true);
-  const [ symbols, setSymbols ] = useState(false);
-  const [ extras, setExtras ] = useState(false);
-  const [ pool, setPool ] = useState("");
-  const [ count, setCount ] = useState<number | undefined>(defaultCount);
-  const [ length, setLength ] = useState<number | undefined>(defaultLength);
-  const [ output, setOutput ] = useState("");
+  const [ upper, setUpper ] = useInputState(true);
+  const [ lower, setLower ] = useInputState(true);
+  const [ numeric, setNumeric ] = useInputState(true);
+  const [ symbols, setSymbols ] = useInputState(false);
+  const [ extras, setExtras ] = useInputState(false);
+  const [ pool, setPool ] = useInputState("");
+  const [ count, setCount ] = useInputState(5);
+  const [ length, setLength ] = useInputState(20);
+  const [ output, setOutput ] = useInputState("");
 
   const generateOutput = () => {
-    const result = generateRandomStrings(length || defaultLength, upper, lower, numeric, symbols, extras ? pool : "", count || defaultCount);
+    const result = generateRandomStrings(length, upper, lower, numeric, symbols, extras ? pool : "", count);
     setOutput(result);
   };
 
   return (
-    <div>
+    <Container fluid>
       <Text sx={asInputLabel} weight={700}>Character Set</Text>
       <Group direction="column" sx={verticalGroupIndent}>
-        <Checkbox
-          checked={upper}
-          label="Uppercase alphabets"
-          onChange={(e) => setUpper(e.currentTarget.checked)}/>
-        <Checkbox
-          checked={lower}
-          label="Lowercase alphabets"
-          onChange={(e) => setLower(e.currentTarget.checked)}/>
-        <Checkbox
-          checked={numeric}
-          label="Digits"
-          onChange={(e) => setNumeric(e.currentTarget.checked)}/>
-        <Checkbox
-          checked={symbols}
-          label="Symbols (!@#$%^&*()[]=)"
-          onChange={(e) => setSymbols(e.currentTarget.checked)}/>
+        <Checkbox checked={upper} label="Uppercase alphabets" onChange={setUpper}/>
+        <Checkbox checked={lower} label="Lowercase alphabets" onChange={setLower}/>
+        <Checkbox checked={numeric} label="Digits" onChange={setNumeric}/>
+        <Checkbox checked={symbols} label="Symbols (!@#$%^&*()[]=)" onChange={setSymbols}/>
         <Group>
-          <Checkbox
-            checked={extras}
-            label="Extra characters"
-            onChange={(e) => setExtras(e.currentTarget.checked)}/>
-          <TextInput
-            placeholder="add more characters"
-            style={{ width: 300 }}
-            value={pool}
-            onChange={(e) => setPool(e.currentTarget.value)}/>
+          <Checkbox checked={extras} label="Extra characters" onChange={setExtras}/>
+          <TextInput placeholder="add more characters" sx={{ width: 300 }} value={pool} onChange={setPool}/>
         </Group>
       </Group>
       <Group sx={defaultMargin} align="end">
@@ -74,8 +53,8 @@ const StringsGenerator = () => {
         minRows={20}
         label={<Text weight={700}>Output</Text>}
         value={output}
-        onChange={(e) => setOutput(e.currentTarget.value)}/>
-    </div>
+        onChange={setOutput}/>
+    </Container>
   );
 };
 
