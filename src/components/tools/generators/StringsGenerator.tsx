ad52ce05efa5aad4b19dd5d36098561c6b0b0f23
SuRu <33urus@gmail.com>
2022-04-10 16:13:19 +0300

    Update StringsGenerator.tsx

--- src/components/tools/generators/StringsGenerator.tsx
+++ src/components/tools/generators/StringsGenerator.tsx
@@ -1,27 +1,30 @@
 import { Button, Checkbox, Group, NumberInput, Text, Textarea, TextInput } from "@mantine/core";
 import { useState } from "react";
 import { generateRandomStrings } from "../../../utils/string-utils";
+import { asInputLabel, defaultMargin, verticalGroupIndent } from "../../../app-sx";
 
 const StringsGenerator = () => {
+  const defaultCount = 5;
+  const defaultLength = 20;
   const [ upper, setUpper ] = useState(true);
   const [ lower, setLower ] = useState(true);
   const [ numeric, setNumeric ] = useState(true);
   const [ symbols, setSymbols ] = useState(false);
   const [ extras, setExtras ] = useState(false);
   const [ pool, setPool ] = useState("");
-  const [ count, setCount ] = useState(1);
-  const [ length, setLength ] = useState(20);
+  const [ count, setCount ] = useState<number | undefined>(defaultCount);
+  const [ length, setLength ] = useState<number | undefined>(defaultLength);
   const [ output, setOutput ] = useState("");
 
   const generateOutput = () => {
-    const result = generateRandomStrings(length, upper, lower, numeric, symbols, extras ? pool : "", count);
+    const result = generateRandomStrings(length || defaultLength, upper, lower, numeric, symbols, extras ? pool : "", count || defaultCount);
     setOutput(result);
   };
 
   return (
     <div>
-      <Text size={"lg"} weight={700}>Character Set:</Text>
-      <Group direction="column" style={{ marginLeft: 25, marginTop: 15 }}>
+      <Text sx={asInputLabel} weight={700}>Character Set</Text>
+      <Group direction="column" sx={verticalGroupIndent}>
         <Checkbox
           checked={upper}
           label="Uppercase alphabets"
@@ -50,17 +53,29 @@ const StringsGenerator = () => {
             onChange={(e) => setPool(e.currentTarget.value)}/>
         </Group>
       </Group>
-      <Group style={{ marginTop: 15 }}>
-        <Text weight={700}>Length:</Text>
-        <NumberInput value={length} min={1} max={100} onChange={setLength}/>
-        <Text weight={700}>Count: </Text>
-        <NumberInput value={count} min={1} max={20} onChange={setCount}/>
-        <Button onClick={() => generateOutput()}>Generate</Button>
+      <Group sx={defaultMargin} align="end">
+        <NumberInput
+          label={<Text weight={700}>Length</Text>}
+          value={length}
+          min={1}
+          max={100}
+          onChange={setLength}/>
+        <NumberInput
+          label={<Text weight={700}>Count</Text>}
+          value={count}
+          min={1}
+          max={20}
+          onChange={setCount}/>
+        <Button
+          onClick={() => generateOutput()}>
+          Generate
+        </Button>
       </Group>
       <Textarea
-        style={{ marginTop: 15 }}
+        spellCheck="false"
+        sx={defaultMargin}
         minRows={20}
-        label="Output"
+        label={<Text weight={700}>Output</Text>}
         value={output}
         onChange={(e) => setOutput(e.currentTarget.value)}/>
     </div>
