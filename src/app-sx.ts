d48d4213efc538be936f244f07b8f60d619ea0f7
SuRu <33urus@gmail.com>
2022-04-10 16:13:16 +0300

    created app-sx

---
+++ src/app-sx.ts
@@ -0,0 +1,13 @@
+import { Sx } from "@mantine/core";
+
+export const asInputLabel: Sx = {
+  marginBottom: 4
+};
+
+export const defaultMargin: Sx = {
+  marginTop: 15
+};
+
+export const verticalGroupIndent: Sx = {
+  marginLeft: 20
+};
