85868651741c80c5bc303abe45838ce850f55cc6
SuRu <33urus@gmail.com>
2022-04-12 01:28:57 +0300

    created random functions

--- src/utils/random-utils.ts
+++ src/utils/random-utils.ts
@@ -1,10 +1,17 @@
 import * as _ from "lodash";
-import { faker } from "@faker-js/faker";
 
 const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const numericLetters = "0123456789";
 const symbolLetters = "!@#$%^&*()[]=";
+
+export const randomNumber = (value: number | { min: number; max: number; }): number => {
+  const fn = (a: number, b: number) => Math.floor(Math.random() * (b - a)) + a;
+
+  return typeof value === "number" ? fn(0, value) : fn(value.min, value.max);
+};
+
+export const chooseRandom = <T>(items: string | T[]): string | T => items[randomNumber(items.length)];
 
 const randomString = (length: number, upper: boolean, lower: boolean, numeric: boolean, symbols: boolean, pool: string): string => {
   let charSet = "";
@@ -26,7 +33,7 @@ const randomString = (length: number, upper: boolean, lower: boolean, numeric: b
     charSet = _.uniq(charSet).join("");
   }
   return _.range(length)
-    .map(() => charSet[faker.datatype.number(charSet.length - 1)])
+    .map(() => chooseRandom(charSet))
     .join("");
 };
 
