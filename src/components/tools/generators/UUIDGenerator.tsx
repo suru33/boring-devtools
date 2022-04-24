06f7ddfa477242bbdaa0cc9ae57dd9c31819fa5f
SuRu <33urus@gmail.com>
2022-04-24 17:04:31 +0300

    created uuid generator

--- src/components/tools/generators/UUIDGenerator.tsx
+++ src/components/tools/generators/UUIDGenerator.tsx
@@ -1,8 +1,40 @@
+import { useInputState } from "@mantine/hooks";
+import { EMPTY_STRING } from "../../../constants";
+import { randomUUIDs } from "../../../utils/random-utils";
+import { Button, Group, NumberInput, Text, Textarea } from "@mantine/core";
+import { defaultMargin, textAreaDefaultRows } from "../../../app-sx";
+
 const UUIDGenerator = () => {
-  return(
-    <div>
-          RandomUUIDGenerator
-    </div>
+
+  const [ count, setCount ] = useInputState(5);
+  const [ output, setOutput ] = useInputState(EMPTY_STRING);
+
+  const generateOutput = () => {
+    const result = randomUUIDs(count);
+    setOutput(result);
+  };
+
+  return (
+    <>
+      <Group align="end">
+        <NumberInput
+          label={<Text weight={700}>Count</Text>}
+          value={count}
+          min={1}
+          max={20}
+          onChange={setCount}/>
+        <Button onClick={() => generateOutput()}>Generate</Button>
+      </Group>
+      <Textarea
+        spellCheck="false"
+        sx={defaultMargin}
+        minRows={textAreaDefaultRows}
+        variant="filled"
+        label={<Text weight={700}>Output</Text>}
+        value={output}
+        onChange={setOutput}>
+      </Textarea>
+    </>
   );
 };
 
