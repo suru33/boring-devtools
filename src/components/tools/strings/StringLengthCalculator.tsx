acaf01c426492d339cf7e9b90570b21ec8b4f6b0
SuRu <33urus@gmail.com>
2022-04-24 18:20:19 +0300

    feat: created string length calculator (#7)

--- src/components/tools/strings/StringLengthCalculator.tsx
+++ src/components/tools/strings/StringLengthCalculator.tsx
@@ -1,8 +1,32 @@
+import { ChangeEvent } from "react";
+import { Checkbox, Text, Textarea } from "@mantine/core";
+import { defaultMargin, textAreaDefaultRows } from "../../../app-sx";
+import { useInputState } from "@mantine/hooks";
+import { EMPTY_STRING } from "../../../constants";
+
 const StringLengthCalculator = () => {
-  return(
-    <div>
-          StringLengthCalculator
-    </div>
+  const [ input, setInput ] = useInputState(EMPTY_STRING);
+  const [ length, setLength ] = useInputState(0);
+  const [ strip, setStrip ] = useInputState(true);
+
+  const onTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const s = e.target.value;
+    setInput(s);
+    setLength(strip ? s.trim().length : s.length);
+  };
+
+  return (
+    <>
+      <Textarea
+        spellCheck="false"
+        minRows={textAreaDefaultRows}
+        label={<Text weight={700}>Input</Text>}
+        value={input}
+        onChange={onTextChange}>
+      </Textarea>
+      <Checkbox sx={defaultMargin} checked={strip} label="Strip" onChange={setStrip}/>
+      <Text weight={700} sx={defaultMargin}>Length: <Text color="blue" inherit component="span">{length}</Text></Text>
+    </>
   );
 };
 
