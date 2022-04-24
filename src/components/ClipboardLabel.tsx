3b73a7113edb3888edcbe32716b050dec6d70c90
SuRu <33urus@gmail.com>
2022-04-24 21:37:17 +0300

    updated clipboard notification colors

--- src/components/ClipboardLabel.tsx
+++ src/components/ClipboardLabel.tsx
@@ -2,16 +2,26 @@ import { useClipboard } from "@mantine/hooks";
 import { showNotification } from "@mantine/notifications";
 import { ActionIcon, Group, Text, Tooltip } from "@mantine/core";
 import { clipboardIcon } from "../resources/icons";
+import { EMPTY_STRING } from "../constants";
 
 const ClipboardLabel = (props: { label: string, clipboardData: string }) => {
   const clipboard = useClipboard({ timeout: 500 });
 
   const onClipboardClicked = () => {
-    clipboard.copy(props.clipboardData);
-    showNotification({
-      message: `${props.label} text copied to clipboard!`,
-      autoClose: 2000
-    });
+    if(props.clipboardData === EMPTY_STRING) {
+      showNotification({
+        message: "Nothing to copy",
+        autoClose: 2000,
+        color: "orange"
+      });
+    } else {
+      clipboard.copy(props.clipboardData);
+      showNotification({
+        message: `${props.label} text copied to clipboard!`,
+        autoClose: 2000,
+        color: "green"
+      });
+    }
   };
 
   return <Group spacing="xs">
