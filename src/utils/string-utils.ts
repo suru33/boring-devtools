ab66b18d2ea9a3ae20fcbe9b9143e5f840f2df70
SuRu <33urus@gmail.com>
2022-04-24 18:56:57 +0300

    added changeCase function

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,4 +1,17 @@
+import { StringCase } from "../types";
 import { EMPTY_STRING, NO_SPACE } from "../constants";
 
 export const reverse = (s: string): string =>
   s === EMPTY_STRING ? EMPTY_STRING : [...s].reverse().join(NO_SPACE);
+
+export const changeCase = (stringCase: StringCase, s: string): string => {
+  if(s === EMPTY_STRING) {
+    return EMPTY_STRING;
+  } else if(stringCase === "lower") {
+    return s.toLowerCase();
+  } else if (stringCase === "upper") {
+    return s.toUpperCase();
+  } else {
+    return s.toLowerCase().replace(/\b(\w)/g, s => s.toUpperCase());
+  }
+};
