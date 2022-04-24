1bdc1e16113ee7e895640c77aba57b9ab3d80b1a
SuRu <33urus@gmail.com>
2022-04-24 18:57:37 +0300

    feat: create string case converter (#9)

--- src/components/tools/strings/StringCaseConverter.tsx
+++ src/components/tools/strings/StringCaseConverter.tsx
@@ -1,8 +1,62 @@
+import { useInputState } from "@mantine/hooks";
+import { EMPTY_STRING } from "../../../constants";
+import { StringCase } from "../../../types";
+import { Radio, RadioGroup, Text, Textarea } from "@mantine/core";
+import { defaultMargin, textAreaDefaultRowsSmall } from "../../../app-sx";
+import { ChangeEvent } from "react";
+import { changeCase } from "../../../utils/string-utils";
+
 const StringCaseConverter = () => {
-  return(
-    <div>
-          StringCaseConverter
-    </div>
+
+  const [ stringCase, setStringCase ] = useInputState("lower");
+  const [ input, setInput ] = useInputState(EMPTY_STRING);
+  const [ output, setOutput ] = useInputState(EMPTY_STRING);
+
+  const updateOutput = (stringCase: StringCase, s: string) => {
+    setOutput(changeCase(stringCase, s));
+  };
+
+  const onStringCaseChanged = (stringCase: string) => {
+    setStringCase(stringCase);
+    updateOutput(stringCase as StringCase, input);
+  };
+
+  const onTextChanged = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const str = e.target.value;
+    setInput(str);
+    updateOutput(stringCase as StringCase, str);
+  };
+
+  return (
+    <>
+      <RadioGroup
+        label={<Text weight={700}>Select string case</Text>}
+        onChange={onStringCaseChanged}
+        value={stringCase}>
+        <Radio value="lower" label="Lower"/>
+        <Radio value="upper" label="Upper"/>
+        <Radio value="title" label="Title"/>
+      </RadioGroup>
+
+      <Textarea
+        spellCheck="false"
+        sx={defaultMargin}
+        minRows={textAreaDefaultRowsSmall}
+        label={<Text weight={700}>Input</Text>}
+        value={input}
+        onChange={onTextChanged}>
+      </Textarea>
+
+      <Textarea
+        readOnly
+        spellCheck="false"
+        sx={defaultMargin}
+        minRows={textAreaDefaultRowsSmall}
+        variant="filled"
+        label={<Text weight={700}>Output</Text>}
+        value={output}>
+      </Textarea>
+    </>
   );
 };
 
