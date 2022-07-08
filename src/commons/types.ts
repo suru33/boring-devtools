78a12d45c1a9909a6d342c197bd387b201f8eaed
SuRu <suru33@users.noreply.github.com>
2022-07-08 21:26:27 +0300

    moved types to commons

--- src/commons/types.ts
+++ src/commons/types.ts
@@ -1,1 +1,16 @@
-export type DateFormatFn = (d: Date) => string
+export type IPv = "v4" | "v6"
+
+export type StringCase =
+  "lower"
+  | "upper"
+  | "capitalize"
+  | "camel"
+  | "kebab"
+  | "snake"
+  | "upper-first"
+  | "lower-first"
+  | "deburr"
+
+export type StringCaseFunction = Record<StringCase, ((s?: string) => string)>;
+
+export type DateFormatFunction = (d: Date) => string
