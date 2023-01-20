65ad820873f74d085f072e4478d776c9e46a68d6
SuRu <33urus+github@gmail.com>
2023-01-20 20:35:59 +0200

    Upgrade libraries (#19)
    * upgrade dependencies
    
    * move to react 18
    
    * cleanup index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+if(container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
