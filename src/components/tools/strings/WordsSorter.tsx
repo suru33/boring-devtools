a896ac2a9831c5998992b203f402927a0c34f123
SuRu <33urus+github@gmail.com>
2023-01-21 18:26:54 +0200

    create component WordsSorter

--- src/components/tools/strings/WordsSorter.tsx
+++ src/components/tools/strings/WordsSorter.tsx
@@ -1,5 +1,36 @@
-const WordsSorter = () =>
-  <div>
-    WordsSorter
-  </div>;
+import { SimpleGrid, Textarea } from "@mantine/core";
+import { useInputState } from "@mantine/hooks";
+import ComponentLabel from "../../ComponentLabel";
+import CopyTextArea from "../../CopyTextArea";
+import { sortLines } from "../../../commons/utils.strings";
+import { textAreaDefaultRows } from "../../../app-sx";
+import { EMPTY_STRING } from "../../../commons/constants";
+
+const WordsSorter = () => {
+  const [ input, setInput ] = useInputState(EMPTY_STRING);
+  const [ output, setOutput ] = useInputState(EMPTY_STRING);
+  const onTextChange = (value: string) => {
+    setInput(value);
+    setOutput(sortLines(value));
+  };
+  return (
+    <SimpleGrid cols={2}>
+      <Textarea
+        spellCheck="false"
+        minRows={textAreaDefaultRows}
+        label={<ComponentLabel text="Input"/>}
+        value={input}
+        onChange={e => onTextChange(e.target.value)}/>
+      <CopyTextArea
+        readOnly
+        spellCheck="false"
+        variant="filled"
+        minRows={textAreaDefaultRows}
+        label={<ComponentLabel text="Output"/>}
+        value={output}
+      />
+    </SimpleGrid>
+  );
+};
+
 export default WordsSorter;
