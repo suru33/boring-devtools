f90c119f7ec5ae8e0ebea1e59f876f81ed80df50
SuRu <33urus+github@gmail.com>
2023-01-23 00:28:38 +0200

    created more types

--- src/commons/types.ts
+++ src/commons/types.ts
@@ -11,6 +11,19 @@ export type StringCase =
   | "lower-first"
   | "deburr"
 
+export type Base64Conv = "text" | "base64"
+
 export type StringCaseFunction = Record<StringCase, ((s?: string) => string)>;
 
 export type DateFormatFunction = (d: Date) => string
+
+export type ToolId =
+  "9d25" | "80fd" | "412a" | "4dd6" | "4bc4" |
+  "4ddc" | "1e00" | "4e38" | "cf0b" | "e347" |
+  "ab86" | "93a8" | "aa72" | "aad8" | "997f"
+
+export interface ToolProps {
+  id: ToolId,
+}
+
+export type TextType = "words" | "sentences" | "paragraphs"
