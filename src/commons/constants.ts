4cbca86105efc87e0ffb415979efc3c0b74c4a01
SuRu <33urus+github@gmail.com>
2023-01-23 00:28:26 +0200

    added all string resources to constants

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -1,20 +1,115 @@
-import { TextProps } from "@mantine/core";
-
-export const NEW_LINE = "\n";
-export const EMPTY_STRING = "";
-export const NO_SPACE = "";
-export const SEARCH = "Search...";
-export const NOTHING_FOUND = "Nothing found...";
-export const UPPERCASE_LETTERS = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
-export const LOWERCASE_LETTERS = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
-export const NUMERIC_LETTERS = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
-export const SYMBOL_LETTERS = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "=" ];
-
-export const MIN_OUTPUT_ITEMS = 1;
-export const MAX_OUTPUT_ITEMS = 100;
-export const OUTPUT_ITEMS = 25;
-export const FULL_PAGE_TEXT_PROPS: TextProps = {
-  size: 50,
-  weight: 900,
-  ta: "center"
+const __ = {
+  newLine: "\n",
+  emptyStr: "",
+  space: " ",
+  timezoneUtc: "UTC",
+  addressSeperator: "\n--\n",
+  settings: {
+    appname: "boring-devtools",
+    version: "1.0.1",
+    fontFamily: "'JetBrains Mono NL', monospace",
+    myURL: "https://suru.im",
+    license: "MIT",
+    licenseURL: "https://github.com/suru33/boring-devtools/blob/master/LICENSE",
+    copyright: "Copyright © 2023 - 2024 Surendra"
+  },
+  charsets: {
+    uppercase: [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ],
+    lowercase: [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ],
+    numeric: [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ],
+    symbol: [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "[", "]", "=" ]
+  },
+  help: {
+    selectTool: "Select a tool from the sidebar"
+  },
+  errmsg: {
+    dateRange: "* start date and time should be < end date time",
+    timezoneGet: "Failed to get your timezone",
+    lessThanMax: "should be < max",
+    pageNotFound: "Page not found"
+  },
+  sk: {
+    theme: "app-color-scheme",
+    startDate: "sd",
+    endDate: "ed",
+    startTime: "st",
+    endTime: "et"
+  },
+  labels: {
+    output: "Output",
+    input: "Input",
+    inputEmpty: "Input is empty",
+    strip: "Strip",
+    length: "Length",
+    text: "Text",
+    base64: "Base64",
+    generate: "Generate",
+    selectCountries: "Select countries",
+    selectLocale: "Select locale",
+    date: "Date",
+    time: "Time",
+    format: "Format",
+    startDate: "Start date",
+    startDateTime: "Start date & time",
+    endDate: "End date",
+    endDateTime: "End date & time",
+    outputTimezone: "Output timezone",
+    timezone: "Timezone",
+    yourTimezone: "Your timezone: ",
+    utcOffset: "UTC Offset",
+    search: "Search...",
+    nothingFound: "Nothing found...",
+    differenceIs: "The difference is:",
+    howManyQ: "How many?",
+    howManyTimesQ: "How many times?",
+    ipVersion: "IP Version",
+    ipv4: "IPv4",
+    ipv6: "IPv6",
+    min: "Min",
+    max: "Max",
+    precision: "Precision",
+    floatValues: "Float values",
+    charset: "Character Set",
+    addMoreChars: "add more characters",
+    stringCase: "String case",
+    uniqueChars: "Unique Characters",
+    palindrome: "Palindrome string",
+    notPalindrome: "Not a palindrome string",
+    now: "Now",
+    allCountriesSelected: "All countries are selected by default",
+    words: "Words",
+    sentences: "Sentences",
+    paragraphs: "Paragraphs",
+    textType: "Text type",
+    credits: "Credits",
+    license: "License",
+    toggleColorScheme: "Toggle color scheme",
+    copied: "Copied",
+    copy: "Copy",
+    timestampSuffixes: [ "Year", "Month", "Day", "Hour", "Minute", "Second" ],
+    charsets: {
+      uppercase: "Uppercase alphabets",
+      lowercase: "Lowercase alphabets",
+      digits: "Digits",
+      symbols: "Symbols (!@#$%^&*()[]=)",
+      extras: "Extra characters"
+    },
+    shortcuts: {
+      mac: [ "⌘ + K", "⌘ + J" ],
+      others: [ "Ctrl + K", "Ctrl + J" ]
+    }
+  },
+  formats: {
+    splitTimestamp: [ "Y;M;D;H;m;s", ";" ],
+    splitTimestampWithTimeZone: [ "YYYY;MM;DD;HH;mm;ss;Z", ";" ],
+    dateTime: "YYYY-MM-DD HH:mm:ss",
+    dateTimeConv: "YYYY-MM-DDTHH:mm:ss",
+    dateTimeWithOutSeconds: "YYYY-MM-DD HH:mm"
+  },
+  shortcuts: {
+    search: "mod + K",
+    toggleTheme: "mod + J"
+  }
 };
+
+export default __;
