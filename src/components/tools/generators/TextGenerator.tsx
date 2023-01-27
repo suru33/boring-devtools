edcc98840877d94d614d133310d54bd7ad22eef8
SuRu <33urus+github@gmail.com>
2023-01-27 21:22:07 +0200

    add slug type to text generator

--- src/components/tools/generators/TextGenerator.tsx
+++ src/components/tools/generators/TextGenerator.tsx
@@ -1,4 +1,4 @@
-import { Button, Group, Select, Stack } from "@mantine/core";
+import { Button, Group, NumberInput, Select, Stack } from "@mantine/core";
 import { useInputState } from "@mantine/hooks";
 import CopyTextArea from "../../CopyTextArea";
 import HowMany, { useHowManyInputState } from "../../HowMany";
@@ -12,11 +12,13 @@ const TextGenerator = (props: ToolProps) => {
   const [ count, setCount ] = useHowManyInputState();
   const [ output, setOutput ] = useEmptyStringInputState();
   const [ textType, setTextType ] = useInputState<TextType>("words");
+  const [ slugCont, setSlugCount ] = useInputState(3);
 
   const typeSelectData = [
     { value: "words", label: __.labels.words },
     { value: "sentences", label: __.labels.sentences },
-    { value: "paragraphs", label: __.labels.paragraphs }
+    { value: "paragraphs", label: __.labels.paragraphs },
+    { value: "slug", label: __.labels.slug }
   ];
 
   return (
@@ -27,8 +29,20 @@ const TextGenerator = (props: ToolProps) => {
           data={typeSelectData}
           value={textType}
           onChange={v => setTextType(v as TextType | "words")}/>
+        {
+          textType === "slug" &&
+            <NumberInput
+              label={<ComponentLabel text={__.labels.wordsCount}/>}
+              min={2}
+              max={30}
+              value={slugCont}
+              onChange={setSlugCount}
+            />
+        }
         <HowMany value={count} onChange={setCount}/>
-        <Button onClick={() => setOutput(randomText(textType, count))}>{__.labels.generate}</Button>
+        <Button onClick={() => setOutput(randomText(textType, count, slugCont))}>
+          {__.labels.generate}
+        </Button>
       </Group>
       <CopyTextArea value={output}/>
     </Stack>
