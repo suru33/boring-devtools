0b52d6fa347e7f68d894b0fdbc83ffe7ff73b354
SuRu <33urus+github@gmail.com>
2023-02-04 12:31:45 +0200

    created storage utils

--- src/commons/utils.storage.ts
+++ src/commons/utils.storage.ts
@@ -1,7 +1,22 @@
 import { IStorageProperties } from "@mantine/hooks/lib/use-local-storage/create-storage";
+import { useLocalStorage } from "@mantine/hooks";
 import { ToolId } from "./types";
+import { deserializeDate, serializeDate } from "./utils.datetime";
+import { numVals } from "../app-sx";
+import __ from "./constants";
 
-export const sp = <T>(args: IStorageProperties<T> & { tid: ToolId }): IStorageProperties<T> => ({
-  ...args,
-  key: `${args.tid}-${args.key}`
-});
+export const useToolPropsStorage = <T>(args: IStorageProperties<T> & { tid: ToolId }) =>
+  useLocalStorage({
+    ...args,
+    key: `${args.tid}-${args.key}`,
+    getInitialValueInEffect: args.getInitialValueInEffect === undefined ? true : args.getInitialValueInEffect
+  });
+
+export const useToolPropEmptyStringStorage = (args: IStorageProperties<string> & { tid: ToolId }) =>
+  useToolPropsStorage<string>({ ...args, defaultValue: "" });
+
+export const useToolPropHowManyStorage = (args: Partial<IStorageProperties<number>> & { tid: ToolId }) =>
+  useToolPropsStorage<number>({ ...args, key: __.sk.howMany, defaultValue: numVals.defaultOutputItems });
+
+export const useToolPropDateStorage = (args: IStorageProperties<Date> & { tid: ToolId }) =>
+  useToolPropsStorage<Date>({ ...args, serialize: serializeDate, deserialize: deserializeDate });
