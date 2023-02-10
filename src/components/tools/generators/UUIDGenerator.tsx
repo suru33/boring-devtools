03bc563be7a79ed3025fee0ef013079a8e4080f3
SuRu <33urus+github@gmail.com>
2023-02-10 20:09:13 +0200

    Update UUIDGenerator.tsx

--- src/components/tools/generators/UUIDGenerator.tsx
+++ src/components/tools/generators/UUIDGenerator.tsx
@@ -1,19 +1,21 @@
 import { Button, Group, Stack } from "@mantine/core";
 import CopyTextArea from "../../CopyTextArea";
-import HowMany, { useHowManyInputState } from "../../HowMany";
+import HowMany from "../../HowMany";
 import { useEmptyStringInputState } from "../../../commons/utils.strings";
 import { ToolProps } from "../../../commons/types";
 import { randomUUIDs } from "../../../commons/utils.random";
+import { useToolPropHowManyStorage } from "../../../commons/utils.storage";
+import { numVals } from "../../../app-sx";
 import __ from "../../../commons/constants";
 
 const UUIDGenerator = (props: ToolProps) => {
-  const [ count, setCount ] = useHowManyInputState();
+  const [ count, setCount ] = useToolPropHowManyStorage({ tid: props.id });
   const [ output, setOutput ] = useEmptyStringInputState();
 
   return (
     <Stack>
       <Group align="end">
-        <HowMany value={count} onChange={setCount}/>
+        <HowMany value={count} onChange={v => setCount(v || numVals.defaultOutputItems)}/>
         <Button onClick={() => setOutput(randomUUIDs(count))}>{__.labels.generate}</Button>
       </Group>
       <CopyTextArea value={output}/>
