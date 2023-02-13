2a965ef3e404ec070bce2b115a59de1251cac581
SuRu <33urus+github@gmail.com>
2023-02-13 18:28:40 +0200

    bump version to 1.0.5

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -6,7 +6,7 @@ const __ = {
   addressSeperator: "\n--\n",
   settings: {
     appname: "boring-devtools",
-    version: "1.0.4",
+    version: "1.0.5",
     fontFamily: "'JetBrains Mono', monospace",
     myURL: "https://suru.im",
     license: "MIT",
